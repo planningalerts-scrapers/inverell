61d84195f592e99c2e992b62250cae2cf1ba8d7a
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 17:09:07 +1000

    Link to a page that works without a session

--- scraper.rb
+++ scraper.rb
@@ -44,7 +44,7 @@ while cont do
         'council_reference' => data[:council_reference],
         'address'           => data[:address],
         'description'       => data[:description],
-        'info_url'          => data[:detail_url],
+        'info_url'          => scraper.base_url,
         'date_scraped'      => Date.today.to_s,
         'date_received'     => data[:date_received],
       }
