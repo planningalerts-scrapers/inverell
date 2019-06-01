70ea6407131ca23c89da4b985898ee52790125a0
Matthew Landauer <mlandauer@gmail.com>
2019-06-02 08:43:24 +1000

    Much more efficient and sensible way of getting all applications in the year

--- scraper.rb
+++ scraper.rb
@@ -2,29 +2,14 @@ require "epathway_scraper"
 
 # Use current year if environment variable isn't set
 ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
-puts "Getting data in year `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PERIOD environment"
+year = ENV['MORPH_PERIOD'].to_i
+puts "Getting data in year `#{year}`, changable via MORPH_PERIOD environment"
 
-scraper = EpathwayScraper::Scraper.new("http://203.49.140.77/ePathway/Production")
-
-page = scraper.agent.get(scraper.base_url)
-page = EpathwayScraper::Page::ListSelect.follow_javascript_redirect(page, scraper.agent)
-page = EpathwayScraper::Page::ListSelect.pick(page, :all)
-
-# a very bad and hackie way to collect DAs
-# basically scan from DA 1 to whatever....
-i = 1
-loop do
-  list = EpathwayScraper::Page::Search.search_for_one_application(page, "DA-#{i}/#{ENV['MORPH_PERIOD']}")
-
-  no_results = 0
-  EpathwayScraper::Page::Index.scrape_index_page(list, scraper.base_url, scraper.agent) do |record|
-    no_results += 1
-    # The state was missing from the address
-    record["address"] += ", NSW"
-    EpathwayScraper.save(record)
-  end
-  break if no_results == 0
-
-  # increase i value and scan the next DA
-  i += 1
+EpathwayScraper.scrape(
+  "http://203.49.140.77/ePathway/Production",
+  list_type: :all_year, year: year
+) do |record|
+  # The state was missing from the address
+  record["address"] += ", NSW"
+  EpathwayScraper.save(record)
 end
