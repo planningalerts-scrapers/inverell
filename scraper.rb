d98b26a76511f67f81783de48fe278e057a79fd8
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:53:10 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "http://203.49.140.77/ePathway/Production",
-  list_type: :all_this_year, state: "NSW"
-)
+EpathwayScraper.scrape_and_save_authority(:inverell)
