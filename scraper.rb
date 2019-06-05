26bdf9b36d8d4f6bfe3812bfee030242d4795a0b
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:01:51 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,6 @@
 require "epathway_scraper"
 
-# Use current year if environment variable isn't set
-ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
-year = ENV['MORPH_PERIOD'].to_i
-puts "Getting data in year `#{year}`, changable via MORPH_PERIOD environment"
-
-EpathwayScraper.scrape(
+EpathwayScraper.scrape_and_save(
   "http://203.49.140.77/ePathway/Production",
-  list_type: :all_year, year: year
-) do |record|
-  # The state was missing from the address
-  record["address"] += ", NSW"
-  EpathwayScraper.save(record)
-end
+  list_type: :all_this_year, state: "NSW"
+)
